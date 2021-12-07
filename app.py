b606bef1d2ba7116f5bdbb3f7c6c3df23e3c8d24
michal <michal.d.gornicki@gmail.com>
2021-12-07 15:02:52 +0000

    updated form template,appconfig and app files

--- app.py
+++ app.py
@@ -1,53 +1,56 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
+import DBcm
+from appconfig import config
 
-@app.get("/")  # HTTP request: GET /
+
+@app.route("/")  # HTTP request: GET /
 def index():
     return render_template(
         "index.html", title="Welcome!", heading="Welcome to my home page!",
     )
 
 
-@app.get("/cv")  # HTTP request: GET /
+@app.route("/cv")  # HTTP request: GET /
 def cv():
     return render_template("cv.html", heading="My CV",)
 
 
-@app.get("/interest")  # HTTP request: GET /
+@app.route("/interest")  # HTTP request: GET /
 def interest():
     return render_template(
         "interest.html", heading="What I like to do away from the computer.",
     )
 
 
-@app.get("/technologies")  # HTTP request: GET /
+@app.route("/technologies")  # HTTP request: GET /
 def technologies():
     return render_template("technologies.html", heading="Amazing Technologies",)
 
 
-@app.get("/technologies/machinelearning")  # HTTP request: GET /
+@app.route("/technologies/machinelearning")  # HTTP request: GET /
 def ml():
     return render_template("machinelearning.html", heading="Machine Learning",)
 
 
-@app.get("/technologies/cloudcomputing")  # HTTP request: GET /
+@app.route("/technologies/cloudcomputing")  # HTTP request: GET /
 def cc():
     return render_template("cloudcomputing.html", heading="Cloud Computing",)
 
 
-@app.get("/technologies/3dprinting")  # HTTP request: GET /
+@app.route("/technologies/3dprinting")  # HTTP request: GET /
 def printing():
     return render_template("3dprinting.html", heading="3D Printing",)
 
 
-@app.get("/message")  # HTTP request: GET /
+@app.route("/message")  # HTTP request: GET /
 def message():
     return render_template("message.html",)
 
 
-@app.get("/form")
+@app.route("/form")
 def display_form():
     return render_template(
         "form.html",
@@ -56,19 +59,22 @@ def display_form():
     )
 
 
-@app.post("/processform")
+@app.route("/processform", methods=["POST"])
 def save_date():
     # python-name = html-name:
-    first_name = request.form["firstName"]
+    first_name = request.form["fname"]
     email = request.form["email"]
     message = request.form["message"]
 
-    # use the python names
-    with open("comments.txt", "a") as sf:
-        print(f"{email}, {message}", file=sf)
-    return render_template(
-        "message.html", name=first_name, heading="So Long, and Thanks for All the Fish",
-    )
+    # saving data into database
+    with DBcm.UseDatabase(config) as db:
+        SQL = """
+            insert into visitors
+            (fname, email,message)
+            values
+            (%s, %s, %s)
+        """
+        db.execute(SQL,())
 
 
 if __name__ == "__main__":
